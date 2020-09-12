a0f9438389fd5296f3c16cd548c4b2e9579796a0
suryasreepada@gmail.com <suryasreepada@gmail.com>
2020-09-12 10:00:31 -0500

    Initial commit from Visual Studio

---
+++ FirstApplication/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FirstApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from First API...";
+        }
+    }
+}
